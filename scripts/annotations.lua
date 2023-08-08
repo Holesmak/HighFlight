423f170cf27a60c0bccd2622ef57a475ceaee2d3
Holesmak <holesmakl@gmail.com>
2023-08-08 15:57:55 +0300

    More annotations

--- scripts/annotations.lua
+++ scripts/annotations.lua
@@ -2,7 +2,8 @@
 
 ---@class forts
 ---@type function
----@return number teamCount Built in function.
+---@see GetTeamCount Built in function.
+---@return number teamCount
 function GetTeamCount()
         local n
         return n
@@ -14,128 +15,187 @@ end
 function ScheduleCall(timeSeconds, func, param)
 end
 ---@type function
+---@see NodeVelocity Built in function.
 ---@param nodeId number
----@return table velocity Built in function.
+---@return table velocity
 function NodeVelocity(nodeId)
         local vel
         return vel
 end
 ---@type function
+---@see GetTeamId Built in function.
 ---@param nodeId number
----@return number teamId Built in function.
+---@return number teamId
 function GetTeamId(nodeId)
         local n
         return n
 end
 ---@type function
----@param posA table Built in function. Draws a debug line
+---@see SpawnLine Built in function. Draws a debug line
+---@param posA table
 ---@param posB table
 ---@param color table
 ---@param timeToFade number
 function SpawnLine(posA, posB, color, timeToFade)
 end
 ---@type function
----@param pos table Built in function. Draws a debug circle
+---@see SpawnCircle Built in function. Draws a debug circle
+---@param pos table
 ---@param radius number
 ---@param color table
 ---@param timeToFade number
 function SpawnCircle(pos, radius, color, timeToFade)
 end
 ---@type function
----@param string string Built in function. Does not convert other types to string
+---@see Log Built in function. Does not convert other types to string
+---@param string string
 function Log(string)
 end
 ---@type function
+---@see GetNodeProjectileSaveName Built in function.
 ---@param projectileNodeId number
----@return string saveName Built in function.
+---@return string saveName
 function GetNodeProjectileSaveName(projectileNodeId)
         local s
         return s
 end
 ---@type function
+---@see NodePosition Built in function.
 ---@param nodeId number
----@return table position Built in function.
+---@return table position 
 function NodePosition(nodeId)
         local pos
         return pos
 end
 ---@type function
+---@see Colour Built in function. Returns colour table
 ---@param r number
 ---@param g number
 ---@param b number
 ---@param a number
----@return table colour Built in function.
+---@return table colour
 function Colour(r, g, b, a)
         local c
         return c
 end
 ---@type function
----@param projectileNodeId number Built in function.
+---@see Red Built in function. Returns red colour table
+---@return table colour
+function Red()
+        local c
+        return c
+end
+---@type function
+---@see White Built in function. Returns white colour table
+---@return table colour
+function White()
+        local c
+        return c
+end
+---@type function
+---@see Blue Built in function. Returns blue colour table
+---@return table colour
+function Blue()
+        local c
+        return c
+end
+---@type function
+---@see Green Built in function. Returns green colour table
+---@return table colour
+function Green()
+        local c
+        return c
+end
+---@type function
+---@see SetMissileTarget Built in function. Sets position which the missile will be targeted at
+---@param projectileNodeId number
 ---@param pos table
 function SetMissileTarget(projectileNodeId, pos)
 end
 ---@type function
+---@see NodeCount Built in function.
 ---@param teamId number
----@return number nodeCount Built in function.
+---@return number nodeCount
 function NodeCount(teamId)
         local n
         return n
 end
 ---@type function
+---@see ProjectileCount Built in function.
 ---@param teamId number
----@return number projectileCount Built in function.
+---@return number projectileCount
 function ProjectileCount(teamId)
         local n
         return n
 end
 ---@type function
+---@see GetNodeId Built in function. Returns global nodeId when given team nodeId
 ---@param teamId number
 ---@param teamNodeId number
----@return number nodeId Built in function. Returns global nodeId when given team nodeId
+---@return number nodeId
 function GetNodeId(teamId, teamNodeId)
         local id
         return id
 end
 ---@type function
+---@see GetProjectileId Built in function. Returns global nodeProjectileId when given team nodeProjectileId
+---@param teamId number
+---@param teamNodeProjectileId number
+---@return number nodeProjectileId
+function GetProjectileId(teamId, teamNodeProjectileId)
+        local id
+        return id
+end
+---@type function
+---@see NodeExists  Built in function.
 ---@param nodeId number
----@return boolean doesExist Built in function.
+---@return boolean doesExist
 function NodeExists(nodeId)
         local b
         return b
 end
 ---@type function
+---@see GetNodeProjectileType Built in function. See types in forts.lua
 ---@param nodeid number
----@return number type Built in function. See types in forts.lua
+---@return number type
 function GetNodeProjectileType(nodeid)
         local n
         return n
 end
 ---@type table
+---@see data Table which is being synced between players. Use for storing variables and clear on OnReset
 data = {}
 ---@type string
+---@see path String which consist of an actual path to the mod's folder. Example of usage: path .. "/scripts/utils.lua"
 path = ""
+---@type table
+---@see ShootableProjectile Table of the projectiles which will be targeted by the AI anti-air
+ShootableProjectile = {}
 
 
 ---@class Vec3
 ---@type function
+---@see Vec3Length Built in function. Returns vector magnitude
 ---@param vector table
----@return number length Built in function. Returns vector magnitude
+---@return number length
 function Vec3Length(vector)
         local n
         return n
 end
 ---@type function
+---@see Vec3 Built in function. Returns vector table made out of 'x' and 'y' axes
 ---@param x number
 ---@param y number
----@return table vector Built in function. Returns vector made out of 'x' and 'y' axes
+---@return table vector
 function Vec3(x, y)
         local vec
         return vec
 end
 ---@type function
+---@see Vec3Dot Built in function. Returns dot product of the two vectors
 ---@param x table
 ---@param y table
----@return number dot Built in function. Returns dot product of the two vectors
+---@return number dot
 function Vec3Dot(x, y)
         local n
         return n
